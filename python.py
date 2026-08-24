c2299bd8a9c80a8be544b25eef9a989cf03de589
rajender <gargee07@gmail.com>
2026-08-24 14:19:01 +0530

    Add department and id fields

--- python.py
+++ python.py
@@ -5,3 +5,17 @@ class students(models.Models):
     name = models.CharField(max_length = 200)
     age = models.CharField()
     section = models.CharField()
+    department = models.CharField()
+    id = models.CharField()
+
+
+
+class department(models.Models):   
+    section = models.CharField()
+    department = models.CharField()
+    id = models.CharField()
+
+
+
+
+
